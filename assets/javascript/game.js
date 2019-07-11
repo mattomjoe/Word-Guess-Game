cca6d50d34368a4aaba91721f129070c64ca749b
Matt Joseph <mattomjoe@yahoo.com>
2019-07-11 00:08:50 -0500

    revise JS based on user/computer choices script demonstrated in r-p-s video

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,16 @@
-//create array of Simpsons characters that can be guessed
-
-var currentWord = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Smithers", "Burns", "Wiggum", "Apu", "Moe", "Skinner", "Barney", "Krusty", "Brockman", "Carl", "Lou", "Milhouse", "Ned"];
-
 //User presses any key to start game
 
 object.onkeyup = function(event){ //taken from r-p-s video
 
 };
 
-//Simpsons name from currentWord array is randomly selected
+//Create an array that lists out all of the word options.
 
-var currentWord = currentWord[Math.floor(Math.random()*currentWord.length)]; //modified from https://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
+var currentWord = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Smithers", "Burns", "Wiggum", "Apu", "Moe", "Skinner", "Barney", "Krusty", "Brockman", "Carl", "Lou", "Milhouse", "Ned"];
 
+//Randomly choose a name from the array. This is the computer's choice for the user to guess.
 
+var computerChoice = currentWord[Math.floor(Math.random() * currentWord.length)];
 
 //Dashes appear below "Current Word", one dash per letter
 
