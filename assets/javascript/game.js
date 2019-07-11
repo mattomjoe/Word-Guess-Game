f63dbe49e196d5e7f3f5c0c4ca98b56402f837a9
Matt Joseph <mattomjoe@yahoo.com>
2019-07-10 22:01:38 -0500

    add double quotes to Simpson name strings in array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //variable array of Simpsons characters for current word
 
-var currentWord = [Homer, Marge, Bart, Lisa, Maggie, Smithers, Burns, Wiggum, Apu, Moe, Skinner, Barney, Krusty, Brockman, Carl, Lou, Milhouse, Ned]
+var currentWord = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Smithers", "Burns", "Wiggum", "Apu", "Moe", "Skinner", "Barney", "Krusty", "Brockman", "Carl", "Lou", "Milhouse", "Ned"];
 
 //User presses any key to start game
 
@@ -9,6 +9,13 @@ object.onkeypress = function(){myScript};
 //Simpsons name from currentWord array is randomly selected
 
 
+
+
+
+//Randomly select word to be guessed
+
+
+}
 
 //Dashes appear below "Current Word", one dash per letter
 
