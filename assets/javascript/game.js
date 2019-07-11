a0dc3b0d830c34fb544ee5fa5df34d25b9f6fcee
Matt Joseph <mattomjoe@yahoo.com>
2019-07-10 21:27:07 -0500

    add comments, currentWord array, and onkeypress to JavaScript

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//variable array of Simpsons characters for current word
+
+var currentWord = [Homer, Marge, Bart, Lisa, Maggie, Smithers, Burns, Wiggum, Apu, Moe, Skinner, Barney, Krusty, Brockman, Carl, Lou, Milhouse, Ned]
+
+//User presses any key to start game
+
+object.onkeypress = function(){myScript};
+
+//Simpsons name from currentWord array is randomly selected
+
+
+
+//Dashes appear below "Current Word", one dash per letter
+
+
+
+//User enters letter guess for currentWord random selection
+
+
+
+//If user entry appears in currentWord
+
