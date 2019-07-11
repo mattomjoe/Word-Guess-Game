8dd3eda220eeda53978ad3b8160fe15c41f8e5d6
Matt Joseph <mattomjoe@yahoo.com>
2019-07-10 22:24:40 -0500

    add press any key to html, change JS for keypress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,18 @@
-//variable array of Simpsons characters for current word
+//create array of Simpsons characters that can be guessed
 
 var currentWord = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Smithers", "Burns", "Wiggum", "Apu", "Moe", "Skinner", "Barney", "Krusty", "Brockman", "Carl", "Lou", "Milhouse", "Ned"];
 
 //User presses any key to start game
 
-object.onkeypress = function(){myScript};
+object.onkeyup = function(event){ //taken from r-p-s video
+
+};
 
 //Simpsons name from currentWord array is randomly selected
 
+var currentWord = currentWord[Math.floor(Math.random()*currentWord.length)]; //modified from https://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
 
 
-
-
-//Randomly select word to be guessed
-
-
-}
 
 //Dashes appear below "Current Word", one dash per letter
 
@@ -23,7 +20,7 @@ object.onkeypress = function(){myScript};
 
 //User enters letter guess for currentWord random selection
 
-
+var userGuess = event.key;
 
 //If user entry appears in currentWord
 
