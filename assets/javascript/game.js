7b8f88532fb12ebf76ad0de368ca71719acf6ad1
Matt Joseph <mattomjoe@yahoo.com>
2019-07-15 20:59:46 -0500

    reformat Simpsons name array in JS code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+
 //User presses any key to start game
 
 object.onkeyup = function(event){ //taken from r-p-s video
 
 };
 
-//Create an array that lists all of the word options.
+    //Create an array that lists all of the word options.
+    var currentWord = [
+        "Homer", 
+        "Marge", 
+        "Bart", 
+        "Lisa", 
+        "Maggie", 
+        "Smithers", 
+        "Burns", 
+        "Wiggum", 
+        "Apu", 
+        "Moe", 
+        "Skinner", 
+        "Barney", 
+        "Krusty", 
+        "Brockman", 
+        "Carl", 
+        "Lou", 
+        "Milhouse", 
+        "Ned"
+    ];
 
-var currentWord = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Smithers", "Burns", "Wiggum", "Apu", "Moe", "Skinner", "Barney", "Krusty", "Brockman", "Carl", "Lou", "Milhouse", "Ned"];
+    //Randomly choose a name from the array. This is the computer's choice for the user to guess.
+    var computerChoice = currentWord[Math.floor(Math.random() * currentWord.length)];
 
-//Randomly choose a name from the array. This is the computer's choice for the user to guess.
+    //Dashes appear below "Current Word", one dash per letter
+    var answerArray = [];
+    for (var i = 0; i < currentWord.length; i++) {
+        answerArray[i] = "_";
+    }
 
-var computerChoice = currentWord[Math.floor(Math.random() * currentWord.length)];
+    var remainingLetters = computerChoice.length;
 
-//Dashes appear below "Current Word", one dash per letter
+    
 
 
 
@@ -25,3 +54,4 @@ document.onkeyup = function(event) {
 
 //If user entry appears in currentWord
 
+})
