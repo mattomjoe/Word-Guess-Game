6c89ac71b0897dfa0661811aacf8879363288bc4
Matt Joseph <mattomjoe@yahoo.com>
2019-07-16 23:03:01 -0500

    bring in JS code from psychic game and begin modifying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-
-
-//User presses any key to start game
-
-object.onkeyup = function(event){ //taken from r-p-s video
-
-};
-
     //Create an array that lists all of the word options.
     var currentWord = [
         "Homer", 
@@ -30,6 +22,30 @@ object.onkeyup = function(event){ //taken from r-p-s video
         "Ned"
     ];
 
+    // Creating variables to hold the number of wins, current word, number of guesses left, and letters already guessed.
+    var wins = 0;
+    var guessesLeft = 15;
+    var guessedSofar = []; // Blank array to hold user guesses
+
+    // Create variables that hold references to the places in HTML where we want to display things.
+    var directionsText = document.getElementById("directions-text");
+    var winsText = document.getElementById("wins-text");
+    var guessesLeftText = document.getElementById("guesses-left-text");
+    var guessSoFarText = document.getElementById("guess-so-far-text");
+
+    function game() {
+        var guessesLeft = 15;
+        guessedSofar = []; // Blank array to hold user guesses
+    }
+
+    //Get user input using key press. Store info in variable for later use.
+    //This function is run whenever the user presses a key.
+    document.onkeyup = function(event){ // onkeyup is the event listener. "event" is not a method, it is an object passed in implicitly.
+        var userGuess = event.key; // Event is being passed, key stroke is the part of the event that is captured. "key" is a property of the object, "event"
+    }
+
+    
+
     //Randomly choose a name from the array. This is the computer's choice for the user to guess.
     var computerChoice = currentWord[Math.floor(Math.random() * currentWord.length)];
 
@@ -41,7 +57,7 @@ object.onkeyup = function(event){ //taken from r-p-s video
 
     var remainingLetters = computerChoice.length;
 
-    
+
 
 
 
